ff65a9ec2bd723dee78584c0866cfacc9f095ae5
Andreas Gohr <gohr@cosmocode.de>
2016-10-27 11:54:53 +0200

    first syntax plugin implementation

--- syntax.php
+++ syntax.php
@@ -7,26 +7,28 @@
  */
 
 // must be run within Dokuwiki
-if (!defined('DOKU_INC')) die();
+if(!defined('DOKU_INC')) die();
 
 class syntax_plugin_kitty extends DokuWiki_Syntax_Plugin {
     /**
      * @return string Syntax mode type
      */
     public function getType() {
-        return 'FIXME: container|baseonly|formatting|substition|protected|disabled|paragraphs';
+        return 'substition';
     }
+
     /**
      * @return string Paragraph type
      */
     public function getPType() {
-        return 'FIXME: normal|block|stack';
+        return 'block';
     }
+
     /**
      * @return int Sort order - Low numbers go before high numbers
      */
     public function getSort() {
-        return FIXME;
+        return 155;
     }
 
     /**
@@ -35,39 +37,40 @@ class syntax_plugin_kitty extends DokuWiki_Syntax_Plugin {
      * @param string $mode Parser mode
      */
     public function connectTo($mode) {
-        $this->Lexer->addSpecialPattern('<FIXME>',$mode,'plugin_kitty');
-//        $this->Lexer->addEntryPattern('<FIXME>',$mode,'plugin_kitty');
+        $this->Lexer->addSpecialPattern('\{\{kitty \d+ \d+\}\}', $mode, 'plugin_kitty');
     }
-
-//    public function postConnect() {
-//        $this->Lexer->addExitPattern('</FIXME>','plugin_kitty');
-//    }
 
     /**
      * Handle matches of the kitty syntax
      *
      * @param string $match The match of the syntax
-     * @param int    $state The state of the handler
-     * @param int    $pos The position in the document
-     * @param Doku_Handler    $handler The handler
+     * @param int $state The state of the handler
+     * @param int $pos The position in the document
+     * @param Doku_Handler $handler The handler
      * @return array Data for the renderer
      */
-    public function handle($match, $state, $pos, Doku_Handler &$handler){
-        $data = array();
+    public function handle($match, $state, $pos, Doku_Handler $handler) {
+        $match = trim(substr($match, 7, -2));
+        list($width, $height) = explode(' ', $match);
+        $width = (int) $width;
+        $height = (int) $height;
 
-        return $data;
+        return array($width, $height);
     }
 
     /**
      * Render xhtml output or metadata
      *
-     * @param string         $mode      Renderer mode (supported modes: xhtml)
-     * @param Doku_Renderer  $renderer  The renderer
-     * @param array          $data      The data from the handler() function
+     * @param string $mode Renderer mode (supported modes: xhtml)
+     * @param Doku_Renderer $renderer The renderer
+     * @param array $data The data from the handler() function
      * @return bool If rendering was successful.
      */
-    public function render($mode, Doku_Renderer &$renderer, $data) {
+    public function render($mode, Doku_Renderer $renderer, $data) {
         if($mode != 'xhtml') return false;
+
+        list($width, $height) = $data;
+        $renderer->doc .= '<img src="https://placekitten.com/' . $width . '/' . $height . '" class="kitty" />';
 
         return true;
     }
